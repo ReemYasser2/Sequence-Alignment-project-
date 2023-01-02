78003142915d9a7fcd5f5025f04fb1d25adbba83
Reem Yasser <76207497+ReemYasser2@users.noreply.github.com>
2023-01-02 22:18:57 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ class Ui(QtWidgets.QMainWindow):
     def get_input_pairwise(self):
 
         # read user input sequences from the gui 
-        seq1_in = self.pairwise_seq1_in_line.text()
-        seq2_in = self.pairwise_seq2_in_line.text()
+        seq1_in = self.pairwise_seq1_in_line.toPlainText()
+        seq2_in = self.pairwise_seq2_in_line.toPlainText()
         # convert the string to a list to deal with more easily in the alignment functions
         seq1 = stringToList(seq1_in)
         seq2 = stringToList(seq2_in)
@@ -32,8 +32,14 @@ class Ui(QtWidgets.QMainWindow):
 
     def get_input_msa(self):
         # read user input sequences from the gui 
-        seq1_in = self.msa_seq1_in_line.text()
-        seq2_in = self.msa_seq2_in_line.text()
+        seq1_in = self.msa_seq1_in_line.toPlainText()
+        seq2_in = self.msa_seq2_in_line.toPlainText()
+        # use muscle to align multiple sequences
+        output = subprocess.check_output(
+        ["F:\Installations\muscle5.1.win64.exe",
+        "-align", r"F:\Personal\UNI\Senior I Fall 2022\SBEN331 - Bioinformatics I\Final Project\Group_1.fasta",
+        "-output", r"F:\Personal\UNI\Senior I Fall 2022\SBEN331 - Bioinformatics I\Final Project\aligned.fasta"],
+        text=True)
         # convert the string to a list to deal with more easily in the alignment functions
         seq1 = stringToList(seq1_in)
         seq2 = stringToList(seq2_in)
@@ -44,9 +50,10 @@ class Ui(QtWidgets.QMainWindow):
         return [seq1, seq2, match, mismatch, gap]
 
     def global_alignment(self):
-        seq1, seq2, match, mismatch, gap = self.get_input_pairwise()
-        seq1 = "agc"
-        seq2 = "aaac"
+        seq1_in, seq2_in, match, mismatch, gap = self.get_input_pairwise()
+        # input sequences
+        seq1 = stringToList(seq1_in)
+        seq2 = stringToList(seq2_in)
         # traceback step directions
         done = 5
         up_dir = 1
@@ -128,8 +135,87 @@ class Ui(QtWidgets.QMainWindow):
         self.pairwise_output_line.setText(str(output))
 
     def local_alignment(self):
-        print(1)
-    
+        seq1_in, seq2_in, match, mismatch, gap = self.get_input_pairwise()
+        # input sequences
+        seq1 = stringToList(seq1_in)
+        seq2 = stringToList(seq2_in)
+        # length of sequences
+        len_seq_1 = len(seq1)
+        len_seq_2 = len(seq2)
+        # length for zero-based indexing
+        matrix_local_num_cols = len_seq_1 + 1 # seq1
+        matrix_local_num_rows = len_seq_2 + 1  # seq2
+        # traceback step directions
+        done = 5
+        up_dir = 1
+        left_dir = 2
+        diag_dir = 3
+        # # TRACEBACK INITIALIZATION
+        traceback_matrix = np.zeros((matrix_local_num_rows, matrix_local_num_cols))
+        traceback_matrix[0,0] = done
+        for j in range(1, matrix_local_num_cols):
+            traceback_matrix[0,j] = left_dir
+        for i in range(1, matrix_local_num_rows):
+            traceback_matrix[i,0] = up_dir
+        # matrix initialization
+        matrix_local = np.zeros((len_seq_2  + 1, len_seq_1  + 1))
+        # Alignment Matrix and Traceback Matrix Fill
+        for i in range(1, len_seq_2 + 1):
+            for j in range(1, len_seq_1 + 1):
+                if seq1[j-1] == seq2[i-1]:
+                    score = match
+                else:
+                    score = mismatch
+                res = max(0, matrix_local[i-1][j-1] + score, matrix_local[i-1][j] + gap, matrix_local[i][j-1] + gap)
+                matrix_local[i][j] = res
+
+                if res == matrix_local[i-1][j-1] + score:
+                    traceback_matrix[i][j] = diag_dir
+                if res == matrix_local[i-1][j] + gap:
+                    traceback_matrix[i][j] = up_dir
+                if res == matrix_local[i][j-1] + gap:
+                    traceback_matrix[i][j] = left_dir
+
+        alignment_score = np.max(matrix_local)
+        start_loc = np.where(matrix_local == alignment_score) 
+        start_loc_col = start_loc[1]
+        start_loc_row = start_loc[0]
+
+        aligned_seq1 = []
+        aligned_seq2 = []
+        i = start_loc_row[0] # rows
+        j = start_loc_col[0] # cols
+
+        while(i > 0 or j > 0):
+            if traceback_matrix[i,j] == diag_dir:
+                aligned_seq1.append(seq1[j-1])
+                aligned_seq2.append(seq2[i-1])
+                i -= 1
+                j -= 1
+            elif traceback_matrix[i,j] == left_dir:
+                aligned_seq1.append(seq1[j-1])
+                aligned_seq2.append("-" )
+                j -= 1
+            elif traceback_matrix[i,j] == up_dir:
+                aligned_seq1.append("-")
+                aligned_seq2.append(seq2[i-1])
+                i -= 1
+            elif traceback_matrix[i,j] == done:
+                break
+        aligned_seq1.reverse()
+        aligned_seq2.reverse()
+        separator = ""
+        output_seq1 = separator.join(aligned_seq1)
+        output_seq2 = separator.join(aligned_seq2)
+        
+        out_score = str(alignment_score)
+        output_seq1 += "\n"
+        output_seq2 += "\n"
+        out_score += "\n"
+
+        output = output_seq1 + output_seq2 + out_score
+        self.pairwise_output_line.setText(str(output))
+
     def multiple_sequence_alignment(self):
         seq1, seq2, match, mismatch, gap = self.get_input_msa()
         output = str(match) + " " +  str(mismatch) + " " + str(gap)
