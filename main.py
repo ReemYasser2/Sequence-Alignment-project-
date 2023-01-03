0186d8ead00c3a32179b863bdf9e13de937b8881
Reem Yasser <76207497+ReemYasser2@users.noreply.github.com>
2023-01-03 21:54:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -317,7 +317,7 @@ class Ui(QtWidgets.QMainWindow):
                 seq = ""
                 for line in file:
                     if line.startswith(">"): 
-                        seq+= "\n\n\n"
+                        seq+= "\n\n"
                         continue
                     seq += line.strip()
                 self.msa_output_line.clear()
