7162ecec06a940a18d74672647276e3216e743a4
Reem Yasser <76207497+ReemYasser2@users.noreply.github.com>
2023-01-04 15:05:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -9,13 +9,52 @@ from collections import Counter
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import pandas as pd
-def MI(sequences,i,j):
-    # direct appplication for the rule 
-    Pi = Counter(sequence[i] for sequence in sequences)
-    Pj = Counter(sequence[j] for sequence in sequences)
-    Pij = Counter((sequence[i],sequence[j]) for sequence in sequences)   
 
-    return sum(Pij[(x,y)]*log(Pij[(x,y)]/(Pi[x]*Pj[y])) for x,y in Pij)
+class my_dictionary(dict): 
+    # __init__ function 
+    def __init__(self): 
+        self = dict()   
+    # Function to add key:value 
+    def add(self, key, value): 
+        self[key] = value 
+
+def return_pair(a,b):
+        for x, y in zip(a, b): 
+            return x,y
+
+def MI(sequences):
+
+    residue_frequency = my_dictionary() 
+    for i in sequences:
+        for residue in i:
+            if residue in residue_frequency:
+                residue_frequency[residue] +=1
+            else:
+                residue_frequency.add(residue,1)
+
+    for i in range(len(sequences[0])):
+        residue = sequences[0][i] + sequences[1][i]
+        if residue in residue_frequency:
+                residue_frequency[residue] +=1
+        else:
+            residue_frequency.add(residue,1)
+
+    total_residues =sum(residue_frequency.values())
+    MI=0
+
+    for i in range(len(sequences)):
+        for j in range(i+1, len(sequences)):
+            x = sequences[i].seq 
+            y = sequences[j].seq 
+            #For percent identity analysis
+            residue1,residue2=return_pair(x,y)
+            p_residue1_residue2 = residue_frequency[residue1+residue2] / total_residues
+            p_residue1 = residue_frequency[residue1] / total_residues
+            p_residue2 = residue_frequency[residue2] / total_residues
+            MI += log(p_residue1_residue2 /(p_residue1*p_residue2))
+
+    normalized_mutual_information = (MI/log(total_residues))
+    return MI, normalized_mutual_information
 
 def stringToList(data):
    return list(data)
@@ -132,6 +171,8 @@ class Ui(QtWidgets.QMainWindow):
         self.lcd_mutual_information.display(0)
         self.lcd_percent_identity.display(0)
         self.lcd_sum_of_pairs.display(0)
+        self.lcdNumber.display(0)
+        
     
     def clear_pairwise_fields(self):
         # clear all fields that had anything in them 
@@ -518,14 +559,14 @@ class Ui(QtWidgets.QMainWindow):
         formatted_PI = "{:.2f}".format(percent_Identity)
         print(formatted_PI)
         
-        out = MI(sequences_matrix,seq_len,seq_len)
+        mutual_information, normalized_mutual_information = MI(sequences)
+        formatted_mi = "{:.2f}".format(mutual_information)
+        formatted_norm_mi = "{:.2f}".format(normalized_mutual_information)
 
-
-
-        formatted_mi = "{:.2f}".format(out)
         
         if flag == True:
             self.lcd_mutual_information.display(formatted_mi)
+            self.lcdNumber.display(formatted_norm_mi)
             self.lcd_percent_identity.display(formatted_PI)
             self.lcd_sum_of_pairs.display(formatted_SOP)
 
